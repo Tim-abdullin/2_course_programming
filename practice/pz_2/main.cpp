568fe7012e1cee6477f214a28c2c639c04c7a4ac
Tim-abdullin <tim_abdullin@mail.ru>
2023-10-05 16:32:49 +0300

    Added exception handling in the main function

--- practice/pz_2/main.cpp
+++ practice/pz_2/main.cpp
@@ -1,6 +1,7 @@
 // code from pz_1
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -89,114 +90,124 @@ public:
 
     // сложение двух массивов
     void sum(DynamicArray &mas) {
-        if (arr_size != mas.get_size()) {
-            throw invalid_argument("Arrays must have the same size for addition");
+        int opLength = arr_size;
+        if (arr_size > mas.get_size()) {
+            opLength = mas.get_size();
         }
-
-        int16_t *tempArr = new int16_t[arr_size];
-        for (int i = 0; i < mas.get_size();i++){
-            tempArr[i] = arr[i] + mas.get_num(i);
+        for (int i = 0; i < opLength; i++){
+            arr[i] += mas.get_num(i);
         }
-
-        memcpy(arr, tempArr, sizeof(int16_t) * arr_size);
-
-        delete [] tempArr;
     }
 
     // вычитание двух массивов
     void sub(DynamicArray &mas) {
-        if (arr_size != mas.get_size()) {
-            throw invalid_argument("Arrays must have the same size for addition");
+        int opLength = arr_size;
+        if (arr_size > mas.get_size()) {
+            opLength = mas.get_size();
         }
-
-        int16_t *tempArr = new int16_t[arr_size];
-        for (int i = 0; i < mas.get_size();i++){
-            tempArr[i] = arr[i] - mas.get_num(i);
+        for (int i = 0; i < opLength; i++){
+            arr[i] -= mas.get_num(i);
         }
-
-        memcpy(arr, tempArr, sizeof(int16_t) * arr_size);
-
-        delete [] tempArr;
     }
 };
 
-int main(){
-    int size_1;
-    cin >> size_1;
-    DynamicArray mas_1(size_1);
-    for (int i = 0; i < size_1; i++){
-        int16_t number;
-        cin >> number;
-        mas_1.set_num(number, i);
-    }
+int main() {
+    try{
+        int size_1;
+        cin >> size_1;
+        if (size_1 < 0) {
+            throw invalid_argument("Array 1 size must be 0 or positive integer");
+        }
+        DynamicArray mas_1(size_1);
+        for (int i = 0; i < size_1; i++) {
+            int16_t number;
+            cin >> number;
+            mas_1.set_num(number, i);
+        }
 
-    int size_2;
-    cin >> size_2;
-    DynamicArray mas_2(size_2);
-    for (int i = 0; i < size_2; i++){
-        int16_t number;
-        cin >> number;
-        mas_2.set_num(number, i);
-    }
+        int size_2;
+        cin >> size_2;
+        if (size_2 < 0) {
+            throw invalid_argument("Array 2 size must be 0 or positive integer");
+        }
+        DynamicArray mas_2(size_2);
+        for (int i = 0; i < size_2; i++) {
+            int16_t number;
+            cin >> number;
+            mas_2.set_num(number, i);
+        }
 
-    int n;
-    cin >> n;
+        int n;
+        cin >> n;
 
-    for (int i = 0; i < n; i++){
-        int command, arr_index;
-        cin >> command >> arr_index;
-        if (arr_index == 1){
-            if (command == 1){
-                int index;
-                cin >> index;
-                cout << mas_1.get_num(index) << endl;
+        for (int i = 0; i < n; i++) {
+            int command, arr_index;
+            cin >> command >> arr_index;
+            if (arr_index == 1) {
+                if (command == 1) {
+                    int index;
+                    cin >> index;
+                    if (index <0 || index >= size_1) {
+                        throw out_of_range("Index out of Array 1 range");
+                    }
+                    cout << mas_1.get_num(index) << endl;
+                } else if (command == 2) {
+                    int index, newValue;
+                    cin >> index >> newValue;
+                    if (index <0 || index >= size_1) {
+                        throw out_of_range("Index out of Array 1 range");
+                    }
+                    mas_1.set_num(newValue, index);
+                } else if (command == 3) {
+                    int newValue;
+                    cin >> newValue;
+                    mas_1.append(newValue);
+                } else if (command == 4) {
+                    mas_1.print_array();
+                } else if (command == 5) {
+                    mas_1.sum(mas_2);
+                } else if (command == 6) {
+                    mas_1.sub(mas_2);
+                }
             }
-            else if (command == 2){
-                int index, newValue;
-                cin >> index >> newValue;
-                mas_1.set_num(newValue, index);
-            }
-            else if (command == 3){
-                int newValue;
-                cin >> newValue;
-                mas_1.append(newValue);
-            }
-            else if (command == 4){
-                mas_1.print_array();
-            }
-            else if (command == 5){
-                mas_1.sum(mas_2);
-            }
-            else if (command == 6) {
-                mas_1.sub(mas_2);
+            if (arr_index == 2) {
+                if (command == 1) {
+                    int index;
+                    cin >> index;
+                    if (index <0 || index >= size_2) {
+                        throw out_of_range("Index out of Array 2 range");
+                    }
+                    cout << mas_2.get_num(index) << endl;
+                } else if (command == 2) {
+                    int index, newValue;
+                    cin >> index >> newValue;
+                    if (index <0 || index >= size_1) {
+                        throw out_of_range("Index out of Array 2 range");
+                    }
+                    mas_2.set_num(newValue, index);
+                } else if (command == 3) {
+                    int newValue;
+                    cin >> newValue;
+                    mas_2.append(newValue);
+                } else if (command == 4) {
+                    mas_2.print_array();
+                } else if (command == 5) {
+                    mas_2.sum(mas_2);
+                } else if (command == 6) {
+                    mas_2.sub(mas_2);
+                }
             }
         }
-        if (arr_index == 2){
-            if (command == 1){
-                int index;
-                cin >> index;
-                cout << mas_2.get_num(index) << endl;
-            }
-            else if (command == 2){
-                int index, newValue;
-                cin >> index >> newValue;
-                mas_2.set_num(newValue, index);
-            }
-            else if (command == 3){
-                int newValue;
-                cin >> newValue;
-                mas_2.append(newValue);
-            }
-            else if (command == 4){
-                mas_2.print_array();
-            }
-            else if (command == 5){
-                mas_2.sum(mas_2);
-            }
-            else if (command == 6){
-                mas_2.sub(mas_2);
-            }
-        }
     }
+    catch (const invalid_argument &e) {
+        cerr << "Invalid argument: " << e.what() << endl;
+    }
+    catch (const out_of_range &e) {
+        cerr << "Out of range: " << e.what() << endl;
+    }
+    catch (const exception &e) {
+        cerr << "Error: " << e.what() << endl;
+    }
+
     return 0;
 }
