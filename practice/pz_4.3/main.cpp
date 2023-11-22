33e7552496663cd245115486172525e69757c36a
Tim-abdullin <tim_abdullin@mail.ru>
2023-11-22 11:51:55 +0300

    Changed code for task 4.3

--- practice/pz_4.3/main.cpp
+++ practice/pz_4.3/main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <stack>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ void print_table (const vector<vector<int>>& table) {
     }
 }
 
-// алгоритм для построения матрицы достижимости (предполагает, что если есть путь от i до k и от k до j, то от i до j есть путь)
+// O(n^3) алгоритм Флойда-Уоршелла для построения матрицы достижимости (предполагает, что если есть путь от i до k и от k до j, то от i до j есть путь)
 void floyd_warshell_algorithm(vector<vector<int>>& table) {
     int n = table.size();
 
@@ -39,12 +40,44 @@ void floyd_warshell_algorithm(vector<vector<int>>& table) {
     print_table(table);
 }
 
+// O(n^2) поиск в глубину
+void depth_search(const map<int, vector<int>>& graph, vector<vector<int>>& table ,int startNode = 1) {
+    table[startNode-1][startNode-1] = 1;    // путь в самого себя
+    stack<int> stack;
+    vector<bool> visited (graph.size(), false);
+
+    stack.push(startNode);
+    visited[startNode] = true;
+
+    while (!stack.empty()) {
+        int current = stack.top();
+        stack.pop();
+
+        for (int neighbor : graph.at(current)) {
+            if (neighbor == 0) {
+                continue;
+            }
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                stack.push(neighbor);
+                table[startNode - 1][neighbor - 1] = 1;
+            }
+        }
+    }
+//    cout << "Таблица изменена" << endl;
+//    for (int i=0; i<table.size(); i++){
+//        for (int j=0; j<table[i].size(); j++)
+//            cout << table[i][j] << ' ';
+//        cout << endl;
+//    }
+}
+
 int main() {
     int n = 0;
     cin >> n;
     string input;
     map<int, vector<int>> graph {};
-    vector<vector<int>> table (n, vector<int> (n));
+    vector<vector<int>> graph_table (n, vector<int> (n));
     cin.ignore();
 
 //  Заполнение графа
@@ -63,14 +96,27 @@ int main() {
 //  Построение таблицы доступных путей для конкретного узла
     for (const auto& pair :graph) {
         int i = pair.first - 1;
-        table[i][i] = 1;    // заполнение пути самого в себя
+        graph_table[i][i] = 1;    // заполнение пути самого в себя
         for (int connectedNode : pair.second) {
             int j = connectedNode - 1;
-            table[i][j] = 1;    // заполнение пути в доступные узлы
+            graph_table[i][j] = 1;    // заполнение пути в доступные узлы
         }
     }
+//    vector<vector<int>> table_save (graph_table);     // сохранение состояния таблицы для будущих изменений
 
-    floyd_warshell_algorithm(table);
+    floyd_warshell_algorithm(graph_table);
+
+    vector<vector<int>> new_table(n, vector<int>(n, 0));
+    for (int i = 1; i < n + 1; i++) {
+        depth_search(graph, new_table, i);
+    }
+
+    cout << endl << "Матрица достижимости по алгоритму Флойда-Уоршелла:" << endl;
+    print_table(graph_table);
+
+
+    cout << endl << "Матрица достижимости по поиску в глубину:" << endl;
+    print_table(new_table);
 
     return 0;
 }
